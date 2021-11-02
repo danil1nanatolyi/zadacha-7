659b53ab6212bf6a1bf6520dde036f2e9c0ed7d0
danil1nanatolyi <80408487+danil1nanatolyi@users.noreply.github.com>
2021-11-02 21:07:28 +0300

    Add files via upload

---
+++ 32/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Введите число:");
+        double number = new Scanner(System.in).nextDouble();
+
+        isInteger(number);
+
+    }
+
+    static void isInteger(double number) {
+        if (number % 1 == 0) System.out.println("Целое число.");
+        else System.out.println("Нецелое число.");
+
+    }
+}
